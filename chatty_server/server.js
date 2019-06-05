9e50c6d877595bdef95566c9a941df769fffef99
jitammy <jitammy@gmail.com>
2019-06-05 18:26:59 +0000

    Displaying Incoming Messages

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const WebSocket = require('ws')
 const SocketServer = require('ws').Server;
+const uuidv1 = require('uuid/v1')
 const PORT = 3001;
 const server = express()
     .use(express.static('public'))
@@ -7,6 +9,24 @@ const server = express()
 const wss = new SocketServer({ server });
 wss.on('connection', (ws) => {
     console.log('Client connected');
+    ws.on('message', message => {
+        const parsedMessage = JSON.parse(message)
 
+        const clientMessage = {
+            type: "incomingMessage",
+            id: uuidv1(),
+            username: parsedMessage.username,
+            content: parsedMessage.content
+        }
+        const jsonToSend = JSON.stringify(clientMessage)
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(jsonToSend);
+            }
+            console.log(client.readyState, WebSocket.OPEN)
+        })
+
+        console.log(JSON.stringify(clientMessage))
+    });
     ws.on('close', () => console.log('Client disconnected'));
 });
--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,48 @@
 import React, { Component } from 'react';
 import MessageList from './MessageList.jsx';
 import ChatBar from './ChatBar.jsx';
+import { join } from 'path';
 class App extends Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this)
     this.state = {
       currentUser: { name: "Bob" },
-      messages: [
-        {
-          id: 1,
-          username: "Bob",
-          content: ""
-        }
-      ]
-    }
-  }
-
-  handleChange(e) {
-    console.log(e.key)
-    const newMessage = {
-      username: this.state.currentUser.name,
-      content: e.target.value
-    };
-    // if person clicked enter then setState
-    if(e.key === "Enter"){
-      const messages = this.state.messages.concat(newMessage)
-      this.setState({ messages: messages })
-      document.getElementById('contentInput').value = ''
+      messages: []
     }
   }
 
   componentDidMount() {
     console.log("componentDidMount <App />");
     this.socket = new WebSocket("ws://localhost:3001")
-
-    this.socket.onopen = function(event){
+    this.socket.onopen = function (event) {
       console.log("Listening on 3001 Client connected")
+      // console.log(event)
     }
+
     setTimeout(() => {
       console.log("Simulating incoming message");
       const newMessage = { id: 3, username: "Michelle", content: "Hello there!" };
       const messages = this.state.messages.concat(newMessage)
       this.setState({ messages: messages })
     }, 1000);
-  };
 
+    this.socket.onmessage = (event) => {
+      let data = JSON.parse(event.data)
+
+     
+      const incomingNewMessage = {
+        type: "incomingMessage",
+        id: data.id,
+        username: data.username,
+        content: data.content
+      }
+
+      const allMessages = this.state.messages.concat(incomingNewMessage)
+      this.setState({ messages: allMessages })
+      console.log(this.state)
+    }
+  };
 
   render() {
     return (
@@ -58,7 +55,22 @@ class App extends Component {
       </div>
     )
   }
+
+
+  handleChange(e) {
+    const newMessage = {
+      type: 'postMessage',
+      username: this.state.currentUser.name,
+      content: e.target.value
+    };
+    // if person clicked enter then setState
+    if (e.key === "Enter") {
+      // const messages = this.state.messages.concat(newMessage)
+      // this.setState({ messages: messages })
+      console.log("this is the message in handleChange", newMessage)
+      this.socket.send(JSON.stringify(newMessage))
+      document.getElementById('contentInput').value = ''
+    }
+  }
 };
-
-
 export default App;
